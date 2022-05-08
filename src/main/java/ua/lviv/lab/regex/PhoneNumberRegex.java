375d6e2adc32d8512e4882365cb6ce10083205ae
Svyatoslav Tsar <79832144+Svts3@users.noreply.github.com>
2022-05-08 14:04:17 +0300

    Changed wrong variable names

--- src/main/java/ua/lviv/lab/regex/PhoneNumberRegex.java
+++ src/main/java/ua/lviv/lab/regex/PhoneNumberRegex.java
@@ -7,18 +7,19 @@ public class PhoneNumberRegex {
 
     public String findAndChangeNumberOnReversed(final String text) {
         Pattern pattern = Pattern.compile(
-                "([\\w\\s]+)?(\\d{3}-\\d{2}-\\d{2}|\\d{7}|\\d{2}-\\d{2}-\\d{2}-\\d{1})(.[\\w\\s]+)?");
+                "([\\w\\s]+)?(\\d{3}-\\d{2}-\\d{2}|\\d{7}|\\d{2}-\\d{2}-\\d{2}-\\d{1})(.[\\w\\s]+)?"
+                );
         Matcher matcher = pattern.matcher(text);
         StringBuilder builder = new StringBuilder();
         if (matcher.matches()) {
             final int firstRegexGroupIndex = 1;
             final int secondRegexGroupIndex = 2;
-            final int thirdGRegexroupIndex = 3;
+            final int thirdRegexroupIndex = 3;
             builder.append(matcher.group(firstRegexGroupIndex) == null ? ""
                     : matcher.group(firstRegexGroupIndex));
             builder.append(new StringBuilder(matcher.group(secondRegexGroupIndex)).reverse());
-            builder.append(matcher.group(thirdGRegexroupIndex) == null ? ""
-                    : matcher.group(thirdGRegexroupIndex));
+            builder.append(matcher.group(thirdRegexroupIndex) == null ? ""
+                    : matcher.group(thirdRegexroupIndex));
 
         } else {
             throw new IllegalArgumentException("The text doesn't follow the regular expression");
--- src/test/java/ua/lviv/lab/regex/TestPhoneNumberRegex.java
+++ src/test/java/ua/lviv/lab/regex/TestPhoneNumberRegex.java
@@ -12,22 +12,22 @@ class TestPhoneNumberRegex {
 
     @Test
     void testFindAndChangeNumberOnReversed() {
-        PhoneNumberRegex base = new PhoneNumberRegex();
+        PhoneNumberRegex phoneNumberRegex = new PhoneNumberRegex();
         Scanner scanner = new Scanner(System.in);
 
         String text = scanner.nextLine();
-        assertEquals("Number is 21-42-542", base.findAndChangeNumberOnReversed(text));
+        assertEquals("Number is 21-42-542", phoneNumberRegex.findAndChangeNumberOnReversed(text));
         text = scanner.nextLine();
-        assertEquals("21-42-542, Tom", base.findAndChangeNumberOnReversed(text));
+        assertEquals("21-42-542, Tom", phoneNumberRegex.findAndChangeNumberOnReversed(text));
         text = scanner.nextLine();
-        assertEquals("21-42-542 Tom", base.findAndChangeNumberOnReversed(text));
+        assertEquals("21-42-542 Tom", phoneNumberRegex.findAndChangeNumberOnReversed(text));
         text = scanner.nextLine();
-        assertEquals("My number is 2142542", base.findAndChangeNumberOnReversed(text));
+        assertEquals("My number is 2142542", phoneNumberRegex.findAndChangeNumberOnReversed(text));
         text = scanner.nextLine();
-        assertEquals("2-14-25-42", base.findAndChangeNumberOnReversed(text));
+        assertEquals("2-14-25-42", phoneNumberRegex.findAndChangeNumberOnReversed(text));
         String text2 = scanner.nextLine();
         assertThrows(IllegalArgumentException.class, () -> {
-            base.findAndChangeNumberOnReversed(text2);
+            phoneNumberRegex.findAndChangeNumberOnReversed(text2);
         });
 
     }
